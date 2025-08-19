c798775eee9399888087b0bf414ef05086c3d06d
milossomers <milossomers@gmail.com>
2025-08-19 08:09:33 +0000

    1

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#demo save
+
 from flask import Flask, request,render_template
 
 app = Flask(__name__)
@@ -11,5 +13,10 @@ def main():
     #database
     return render_template("main.html")
 
+@app.route("/dbs",methods=["GET", "POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return render_template("dbs.html", r=(-50.6*q)+90.2)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=1234)
